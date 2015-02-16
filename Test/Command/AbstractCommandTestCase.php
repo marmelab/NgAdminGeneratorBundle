d9e694f0ab7bf9d3e10dc32b314980341787b984
Jonathan Petitcolas <petitcolas.jonathan@gmail.com>
2015-02-16 12:12:50 +0100

    Functional test command

---
+++ Test/Command/AbstractCommandTestCase.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace marmelab\NgAdminGeneratorBundle\Test\Command;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Bundle\FrameworkBundle\Console\Application;
+use Symfony\Component\Console\Input\StringInput;
+use Symfony\Component\Console\Output\StreamOutput;
+use Symfony\Bundle\FrameworkBundle\Client;
+
+/**
+ * @see http://alexandre-salome.fr/blog/Test-your-commands-in-Symfony2
+ */
+abstract class AbstractCommandTestCase extends WebTestCase
+{
+    public function runCommand(Client $client, $command)
+    {
+        $application = new Application($client->getKernel());
+        $application->setAutoExit(false);
+
+        $fp = tmpfile();
+        $input = new StringInput($command);
+        $output = new StreamOutput($fp);
+
+        $application->run($input, $output);
+
+        fseek($fp, 0);
+        $output = '';
+        while (!feof($fp)) {
+            $output = fread($fp, 4096);
+        }
+        fclose($fp);
+
+        return $output;
+    }
+}
